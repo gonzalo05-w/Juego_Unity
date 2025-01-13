b93fff016040aaf1705f3ea99d98b97c50346133
gonzalo05-w <gonzalobretongarcia05@gmail.com>
2025-01-13 11:13:47 +0100

    new

--- Proyecto/Assets/Scripts/GameManager.cs
+++ Proyecto/Assets/Scripts/GameManager.cs
@@ -1,14 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public int PuntosTotales{ get {return puntosTotales;}}
+
+    public int PuntosTotales { get { return puntosTotales; } }
     private int puntosTotales;
-    
-    public void SumarPuntos(int puntosASumar){
-        puntosTotales+= puntosTotales + puntosASumar;
+
+    public void SumarPuntos(int puntosASumar)
+    {
+        puntosTotales += puntosASumar;
         Debug.Log(puntosTotales);
     }
-    
-
 }
